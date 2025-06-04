163de2f881f8f623211579aa2c1b19dcac92600f
haibac222 <haibac222@gmail.com>
2025-06-05 04:10:50 +0700

    Delete src directory

--- src/supabase.js
+++
@@ -1,9 +0,0 @@
-// src/supabase.js
-import { createClient } from '@supabase/supabase-js';
-
-// Thay thế URL và Key của bạn ở đây
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
-const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
-const supabase = createClient(supabaseUrl, supabaseKey);
-
-export default supabase;
